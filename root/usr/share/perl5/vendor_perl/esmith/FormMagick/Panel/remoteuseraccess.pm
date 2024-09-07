f579aa9b88b400502c49b06447dbfdf9d971c3b0
Trevor Batley <trevor@batley.id.au>
2024-09-07 21:00:51 +1000

    initial commit of file from CVS for smeserver-remoteuseraccess on Sat Sep  7 21:00:51 AEST 2024

---
+++ root/usr/share/perl5/vendor_perl/esmith/FormMagick/Panel/remoteuseraccess.pm
@@ -0,0 +1,421 @@
+#----------------------------------------------------------------------
+#  remoteuseraccess.pm
+#  support@dungog.net
+#----------------------------------------------------------------------
+
+package esmith::FormMagick::Panel::remoteuseraccess;
+
+use strict;
+use warnings;
+
+use esmith::util;
+use esmith::FormMagick;
+use esmith::AccountsDB;
+use esmith::ConfigDB;
+
+use Exporter;
+use Carp qw(verbose);
+
+use HTML::Tabulate;
+
+our @ISA = qw(esmith::FormMagick Exporter);
+
+our @EXPORT = qw();
+
+our $db  = esmith::ConfigDB->open();
+our $adb = esmith::AccountsDB->open();
+
+our $PanelUser = $ENV{'REMOTE_USER'} ||'';
+$PanelUser = $1 if ($PanelUser =~ /^([a-z][\-a-z0-9]*)$/);
+
+sub new {
+    shift;
+    my $self = esmith::FormMagick->new();
+    $self->{calling_package} = (caller)[0];
+    bless $self;
+    return $self;
+}
+
+#server-manager functions
+sub user_accounts_exist
+{
+    return scalar $adb->users;
+}
+
+sub print_table
+{
+    my $self = shift;
+    my $q = $self->{cgi};
+
+    my $user_table =
+    {
+       title => $self->localise('USER_LIST_CURRENT'),
+
+       stripe => '#D4D0C8',
+
+       fields => [ qw(User FullName shell sudo keys chroot vpn Modify) ],
+
+       labels => 1,
+
+       field_attr => {
+                       User          => {
+                                           label_escape => 0,
+                                           label => $self->localise('ACCOUNT')
+                                        },
+                       FullName      => {
+                                           label_escape => 0,
+                                           label => $self->localise('USER_NAME')
+                                        },
+                       shell         => {
+                                           label_escape => 0,
+                                           label => $self->localise('SHELL_ACCESS')
+                                        },
+                       sudo          => {
+                                           label_escape => 0,
+                                           label => $self->localise('SUDO')
+                                        },
+                       keys          => {
+                                           label_escape => 0,
+                                           label => $self->localise('SSH_KEYS')
+                                        },
+                       chroot        => {
+                                           label_escape => 0,
+                                           label => $self->localise('CHROOT_PATH')
+                                        },
+                       vpn           => {
+                                           label_escape => 0,
+                                           label => $self->localise('VPN_ACCESS')
+                                        },
+                       Modify        => {
+                                           label_escape => 0,
+                                           label => $self->localise('MODIFY'),
+                                           link  => \&modify_link
+                                        },
+                      }
+    };
+
+    my @data = ();
+    my @users = $adb->users;
+
+    return $self->localise("ACCOUNT_USER_NONE") if (@users == 0);
+
+    for my $user (@users)
+    {
+        my $username = $user->key;
+        # make clearer by only showing yes and localise
+        my $vpn = $user->prop('VPNClientAccess') || '';
+        if ($vpn eq 'yes') { $vpn = 'YES'; } else { $vpn = ''; }
+        my $sudo = $user->prop('Sudoer') || '';
+        if ($sudo eq 'yes') { $sudo = 'YES'; } else { $sudo = ''; }
+        my $keys = '';
+        my $file = "/home/e-smith/files/users/$username/.ssh/authorized_keys2";
+        if (( -e $file ) && (! -z $file ))
+        { $keys = 'YES'; }
+
+        my $shell = $user->prop('Shell') || '';
+        if ($shell eq '/usr/bin/rssh')  { $shell = ''; }
+
+        my $ChrootDir  = $user->prop('ChrootDir')  || "";
+        $ChrootDir =~ s:/home/e-smith/files/ibays/::;
+        $ChrootDir =~ s:/home/e-smith/files/users/$username/home:home:;
+
+        push @data,
+            { User            => $user->key,
+              FullName        => $user->prop('FirstName') . " " .
+                                 $user->prop('LastName'),
+              Sudoer          => $user->prop('Sudoer') || 'no',
+              VPNClientAccess => $user->prop('VPNClientAccess') || 'no',
+              shell           => $shell,
+              chroot          => $ChrootDir,
+              ChrootDir       => $user->prop('ChrootDir')  || "/home/e-smith/files/users/$username/home",
+              sudo            => $self->localise($sudo),
+              keys            => $self->localise($keys),
+              vpn             => $self->localise($vpn),
+              Modify          => $self->localise('MODIFY'),
+            }
+    }
+
+    my $t = HTML::Tabulate->new($user_table);
+
+    $t->render(\@data, $user_table);
+}
+
+sub modify_link
+{
+    my ($data_item, $row, $field) = @_;
+
+    return "remoteuseraccess?" .
+        join("&",
+        "page=0",
+        "page_stack=",
+        "Next=Next",
+        "User="     .        $row->{User},
+        "FullName=" .        $row->{FullName},
+        "Sudoer=" .          $row->{Sudoer},
+        "Shell=" .           $row->{shell},
+        "ChrootDir=" .       $row->{ChrootDir},
+        "VPNClientAccess=" . $row->{VPNClientAccess},
+        "wherenext=PAGE_MODIFY");
+}
+
+sub get_chroot_dir
+{
+    my $self = shift;
+    my %existingAccounts = ('' => '',
+                            'home' => ". ~/home" ,
+                            '/home/e-smith/files'=> '.. /home/e-smith/files' );
+
+    foreach my $account ($adb->get_all)
+    {
+        if ($account->prop('type') =~ /(ibay)/)
+        {
+            $existingAccounts{"/home/e-smith/files/ibays/" . $account->key} = $account->key;
+            $existingAccounts{"/home/e-smith/files/ibays/" . $account->key . "/html"} = $account->key . "/html";
+            $existingAccounts{"/home/e-smith/files/ibays/" . $account->key . "/files"} = $account->key. "/files";
+        }
+     }
+
+    return(\%existingAccounts);
+}
+
+sub get_keys_text
+{
+    my $self = shift;
+    my $q = $self->{cgi};
+
+    my $user = $q->param('User');
+
+    my $file = "/home/e-smith/files/users/$user/.ssh/authorized_keys2";
+
+    my $sshKeys = '';
+    # if exists and is not empty
+    if (( -e $file ) && (! -z $file ))
+    {
+        open (SSHKEY, "<$file")
+          or die "Error: Could not open file: $file\n";
+	    my @sshTemp = <SSHKEY>;
+	    $sshKeys = join ("", @sshTemp);
+
+	    close SSHKEY;
+    }
+
+    return $sshKeys;
+}
+
+sub change_settings
+{
+    my $self = shift;
+    my $q = $self->{cgi};
+    my $Shell           = $q->param ('Shell');
+    my $Sudoer          = $q->param ('Sudoer');
+    my $VPNClientAccess = $q->param ('VPNClientAccess');
+    my $ChrootDir       = $q->param ('ChrootDir');
+    my $ChrootDir2      = $q->param ('ChrootDir2');
+
+    my $user = $q->param('User');
+
+    if ($user =~ /^([a-z][\-a-z0-9]*)$/)
+    {
+	   $user = $1;
+    }
+
+    my $sshKeys = $q->param ('sshKeys');
+    if ($sshKeys ne '')
+    {
+        my $file = "/home/e-smith/files/users/$user/.ssh/authorized_keys2";
+        my $dir = "/home/e-smith/files/users/$user/.ssh";
+
+        # delete .ssh/authorized_keys2
+        if ($sshKeys =~ /deletekeys/)
+        {
+            system ("/bin/rm -rf $file") == 0
+              or die ("Error deleting $file.\n");
+        }
+        else
+        {
+            # Strip out DOS Carriage Returns (CR)
+            $sshKeys =~ s/\r//g;
+
+	        unless ( -e $file )
+	        { system ("/bin/mkdir -p $dir") == 0
+                  or die ("Error creating ssh directory.\n");  }
+
+            unlink $file;
+
+            open (SSHKEY, ">$file")
+	         or die ("Error saving SSH Keys.\n");
+
+            print SSHKEY "$sshKeys\n";
+            close SSHKEY;
+
+            system ("/bin/chown -R $user.$user $dir") == 0
+              or die ("Error chown .ssh directory.\n");
+        }
+    }
+
+    # if the drop down is blank and field is valid
+    # set the field to the drop down, save the drop down
+    if (($ChrootDir2 eq '' ) && ($ChrootDir ne ''))
+    {
+       $ChrootDir2 = $ChrootDir || '';
+    }
+
+    if (($Sudoer eq 'yes') or ($Shell eq '/bin/bash'))
+    {
+      $adb->set_prop($user, 'Shell', '/bin/bash');
+    } else {
+      $adb->set_prop($user, 'Shell', '/usr/bin/rssh');
+    }
+
+    $adb->set_prop($user, 'Sudoer', $Sudoer);
+    $adb->set_prop($user, 'ChrootDir', $ChrootDir2);
+    $adb->set_prop($user, 'VPNClientAccess', $VPNClientAccess);
+
+    system ("/sbin/e-smith/signal-event", "user-modify", $user) == 0
+        or die ("Error occurred updating user access\n");
+
+    return $self->success("SUCCESS");
+}
+
+#userpanel functions
+sub get_panel_user
+{
+    return $PanelUser;
+}
+
+sub get_full_name
+{
+    return  $adb->get_prop($PanelUser, "FirstName") . " " .
+            $adb->get_prop($PanelUser, "LastName");
+}
+
+sub userpanel_keys_text
+{
+    my $self = shift;
+    my $q = $self->{cgi};
+
+    my $file = "/home/e-smith/files/users/$PanelUser/.ssh/authorized_keys2";
+
+    if ($PanelUser eq 'admin')
+    {  $file = "/root/.ssh/authorized_keys2"; }
+
+	my $sshKeys = '';
+    # if exists and is not empty
+    if (( -e $file ) && (! -z $file ))
+    {
+        open (SSHKEY, "<$file")
+          or die "Error: Could not open file: $file\n";
+	    my @sshTemp = <SSHKEY>;
+	    $sshKeys = join ("", @sshTemp);
+
+	    close SSHKEY;
+    }
+
+    return $sshKeys;
+}
+
+sub userpanel_change_settings
+{
+    my $self = shift;
+    my $q = $self->{cgi};
+    my $Shell           = $q->param ('Shell');
+    my $Sudoer          = $q->param ('Sudoer');
+    my $VPNClientAccess = $q->param ('VPNClientAccess');
+    my $ChrootDir       = $q->param ('ChrootDir');
+    my $ChrootDir2      = $q->param ('ChrootDir2');
+
+    my $user      = $PanelUser;
+
+    if ($user =~ /^([a-z][\-a-z0-9]*)$/)
+    {
+	   $user = $1;
+    }
+
+    my $sshKeys = $q->param ('sshKeys');
+    if ($sshKeys ne '')
+    {
+        my $file = "/home/e-smith/files/users/$user/.ssh/authorized_keys2";
+        my $dir = "/home/e-smith/files/users/$user/.ssh";
+        if ($user eq 'admin')
+        {  $file = "/root/.ssh/authorized_keys2";
+           $dir = "/root/.ssh"; }
+
+        # delete .ssh/authorized_keys2
+        if ($sshKeys =~ /deletekeys/)
+        {
+            system ("/bin/rm -rf $file") == 0
+              or die ("Error deleting $file.\n");
+        }
+        else
+        {
+            # Strip out DOS Carriage Returns (CR)
+            $sshKeys =~ s/\r//g;
+
+	        unless ( -e $file )
+	        { system ("/bin/mkdir -p $dir") == 0
+                  or die ("Error creating ssh directory.\n");  }
+
+            unlink $file;
+
+            open (SSHKEY, ">$file")
+	         or die ("Error saving SSH Keys.\n");
+
+            print SSHKEY "$sshKeys\n";
+            close SSHKEY;
+
+            system ("/bin/chown -R $user.$user $dir") == 0
+              or die ("Error chown .ssh directory.\n");
+        }
+    }
+
+    # if the drop down is blank and field is valid
+    # set the field to the drop down, save the drop down
+    if (($ChrootDir2 eq '' ) && ($ChrootDir ne ''))
+    {
+       $ChrootDir2 = $ChrootDir || '';
+    }
+
+    if (($Sudoer eq 'yes') or ($Shell eq '/bin/bash'))
+    {
+      $adb->set_prop($user, 'Shell', '/bin/bash');
+    } else {
+      $adb->set_prop($user, 'Shell', '/usr/bin/rssh');
+    }
+
+    $adb->set_prop($user, 'Sudoer', $Sudoer);
+    $adb->set_prop($user, 'ChrootDir', $ChrootDir2);
+    $adb->set_prop($user, 'VPNClientAccess', $VPNClientAccess);
+
+    system ("/sbin/e-smith/signal-event", "user-modify", $user) == 0
+        or die ("Error occurred updating user access\n");
+
+    return $self->success("SUCCESS");
+}
+
+sub CheckChrootDirExists
+{
+    my $self = shift;
+    my $q = $self->{cgi};
+
+    my $ChrootDir   = $q->param ('ChrootDir')  || '';
+    my $ChrootDir2  = $q->param ('ChrootDir2') || '';
+
+    if ($ChrootDir2 eq '') 
+    {
+      if ($ChrootDir eq '')
+      {
+        return "CHROOT_PATH_NOT_GIVEN"; 
+      }
+      else
+      {  
+        if ((-e $ChrootDir ) || ($ChrootDir eq 'home'))
+        { return "OK"; }
+        else
+        { return "CHROOT_PATH_NON_EXISTANT"; }
+      } 
+    }
+    else
+    { return "OK"; }
+}
+
+1;
---
+++ smeserver-remoteuseraccess.spec
@@ -0,0 +1,284 @@
+# $Id: smeserver-remoteuseraccess.spec,v 1.5 2021/08/23 07:42:21 terryfage Exp $
+# Authority: dungog
+# Name: Stephen Noble
+
+Summary: Smeserver module to configure Shell and FTP access for individual users
+%define name smeserver-remoteuseraccess
+Name: %{name}
+%define version 1.3
+%define release 7
+Version: %{version}
+Release: %{release}%{?dist}
+License: GNU GPL version 2
+URL: http://www.dungog.net/sme
+Group: Networking/Daemons
+Source: %{name}-%{version}.tar.xz
+
+BuildRoot: /var/tmp/e-smith-buildroot
+BuildRequires: e-smith-devtools
+BuildArchitectures: noarch
+Requires: e-smith-release >= 7
+Requires: e-smith-proftpd >= 1.12.0-6
+Requires: e-smith-openssh >= 1.11.0-24
+Requires: e-smith-formmagick >= 1.4.0-12
+Obsoletes: dungog-proftpd-chroot
+Obsoletes: e-smith-usershellaccess
+Obsoletes: smeserver-sshkeys
+AutoReqProv: no
+
+%description
+SME server enhancement to provide a server-manager panel under security
+where you can enable shell access on a user by user basis.
+
+Shells available bash, optionally with sudo
+or rssh which allows scp sftp rsync cvs rdist
+TODO Chroot RSSH shell access.
+
+Also sets Chroots FTP access, VPN Client Access, 
+and allows you to enter ssh keys  
+
+%changelog
+* Sat Sep 07 2024 cvs2git.sh aka Brian Read <brianr@koozali.org> 1.3-7.sme
+- Roll up patches and move to git repo [SME: 12338]
+
+* Sat Sep 07 2024 BogusDateBot
+- Eliminated rpmbuild "bogus date" warnings due to inconsistent weekday,
+  by assuming the date is correct and changing the weekday.
+  Sat Mar 07 2008 --> Sat Mar 01 2008 or Fri Mar 07 2008 or Sat Mar 08 2008 or ....
+  Thu May 21 2008 --> Thu May 15 2008 or Wed May 21 2008 or Thu May 22 2008 or ....
+
+* Mon Aug 23 2021 Terry Fage <terry.fage@gmail.com> 1.3-6.sme
+- apply locale 2021-08-23 patch
+
+* Wed Feb 24 2021 Jean-Philipe Pialasse <tests@pialasse.com> 1.3-5.sme
+- fix encoding issue in table display [SME: 11396]
+
+* Wed Feb 24 2021 Jean-Philipe Pialasse <tests@pialasse.com> 1.3-4.sme
+- add empty -update event [SME: 11056]
+
+* Mon Oct 26 2020 Brian Read <brianr@bjsystems.co.uk> 1.3-3.sme
+- Initial import to SME10 tree [SME: 11056]
+
+* Wed Mar 09 2016 JP Pialasse <tests@pialasse.com> 1.3-2.sme
+- apply locale 2016-03-09 patch
+
+* Tue Nov 12 2013 Daniel Berteaud <daniel@firewall-services.com> 1.3-1.sme
+- Rebuild for SME9
+
+* Sun Jul 14 2013 JP Pialasse <tests@pialasse.com> 1.2-38.sme
+- apply locale 2013-07-14 patch
+
+* Sun Mar 06 2011 SME Translation Server <translations@contribs.org> 1.2-37.sme
+- apply locale 2011-03-06 patch
+
+* Tue Mar 02 2010 SME Translation Server <translations@contribs.org> 1.2-36.sme
+- apply locale 2010-03-02 patch
+
+* Tue Oct 27 2009 SME Translation Server <translations@contribs.org> 1.2-35.sme
+- apply locale 2009-10-27 patch
+
+* Mon Aug 24 2009 SME Translation Server <translations@contribs.org> 1.2-34.sme
+- apply locale 2009-08-24 patch
+
+* Sat Jun 13 2009 Stephen Noble <support@dungog.net> 1.2-33
+- chroot path not displayed in panel [SME: 5360]
+
+* Mon Apr 27 2009 SME Translation Server <translations@contribs.org> 1.2-32
+- apply locale 2009-04-27 patch
+
+* Sun Mar  1 2009 Jonathan Martens <smeserver-contribs@snetram.nl> 1.2-30
+- Apply  1 Mar 2009 locale patch [SME: 5018]
+
+* Thu Jan  1 2009 Jonathan Martens <smeserver-contribs@snetram.nl> 1.2-29
+- Apply  1 Jan 2009 locale patch [SME: 4900]
+
+* Sun Nov 30 2008 Jonathan Martens <smeserver-contribs@snetram.nl> 1.2-28
+- Apply 30 Nov 2008 locale patch
+
+* Tue Oct 14 2008 Jonathan Martens <smeserver-contribs@snetram.nl> 1.2-27
+- Apply 14 Oct 2008 locale patch
+
+* Sat Sep 27 2008 Stephen Noble <support@dungog.net> - 1.2-26
+- Apply locale patch
+
+* Tue Jul 1 2008 Jonathan Martens <smeserver-contribs@snetram.nl> 1.2-25
+- Apply 1 July 2008 locale patch
+
+* Wed May 21 2008 Jonathan Martens <smeserver-contribs@snetram.nl> 1.2-24
+  Thu May 21 2008 --> Thu May 15 2008 or Wed May 21 2008 or Thu May 22 2008 or ....
+- Apply 21 May 2008 locale patch
+
+* Mon May 5 2008 Jonathan Martens <smeserver-contribs@snetram.nl> 1.2-23
+- Apply 5 May 2008 locale patch
+
+* Sat Apr 26 2008 Jonathan Martens <smeserver-contribs@snetram.nl> 1.2-22
+- Add common <base> tags to e-smith-formmagick's general
+
+* Tue Apr 22 2008 Jonathan Martens <smeserver-contribs@snetram.nl> 1.2-21
+- Forgot to apply patch
+
+* Tue Apr 22 2008 Jonathan Martens <smeserver-contribs@snetram.nl> 1.2-20
+- Apply 22 April 2008 locale patch
+
+* Tue Apr 1 2008 Shad L. Lords <slords@mail.com> 1.2-19
+- Update to UTF-8 translations
+
+* Tue Mar 11 2008 Stephen Noble <support@dungog.net> - 1.2-18
+- update locale 2008-03-11
+
+* Sat Mar 07 2008 Stephen Noble <support@dungog.net> - 1.2-17
+- prepare en lexicons for pootle translations
+
+* Fri Jan 11 2008 Stephen Noble <support@dungog.net> 1.2-16
+- remove duplicate event rssh.conf [SME 3470]
+
+* Thu Dec 20 2007 Stephen Noble <support@dungog.net>
+- french menu fix 1.2-14
+
+* Thu Dec 20 2007 Stephen Noble <support@dungog.net>
+- french translation, thanks Sylvain 1.2-13
+
+* Wed Jun 13 2007 Stephen Noble <support@dungog.net>
+- apply changes from 1.2-2 to 1.2-12
+
+* Sun Apr 29 2007 Shad L. Lords <slords@mail.com>
+- Clean up spec so package can be built by koji/plague
+
+* Sun Feb 18 2007 Stephen Noble <support@dungog.net>
+- swedish translation, thanks Chrille
+- [1.2-12]
+
+* Tue Jan 23 2007 Stephen Noble <support@dungog.net>
+- spanish translation, thanks Gene
+- requires e-smith-proftpd >= 1.12.0-6, custom template removed
+- default set, ftp{ChrootDir} = home
+- [1.2-11]
+
+* Thu Dec 07 2006 Shad L. Lords <slords@mail.com>
+- Update to new release naming.  No functional changes.
+- Make Packager generic
+
+* Thu Nov 23 2006 Stephen Noble <support@dungog.net>
+- chroot path fix when home set [sme 2084]
+- [1.2-10]
+
+* Fri Nov 10 2006 Stephen Noble <support@dungog.net>
+- fix textarea bug  [sme 1088]
+- requires perl-CGI-FormMagick-0.92-08
+- [1.2-9]
+
+* Fri Nov 10 2006 Stephen Noble <support@dungog.net>
+- chroot path fix [sme 2046]
+- [1.2-8]
+
+* Fri Nov 3 2006 Stephen Noble <support@dungog.net>
+- german translation fix
+- [1.2-7]
+
+* Fri Aug 25 2006 Stephen Noble <support@dungog.net>
+- empty chroot path defaults to home [sme 1249]
+- [1.2-6]
+
+* Thu Aug 24 2006 Stephen Noble <support@dungog.net>
+- now finds correct sshkeys when used under userpanel
+- [1.2-5]
+
+* Wed May 10 2006 Stephen Noble <support@dungog.net>
+- German translation fixed [sme 1258]
+- [1.2-4]
+
+* Wed May 10 2006 Stephen Noble <support@dungog.net>
+- RSSH access uses VPNAccess property
+- German translation added, thanks Dietmar
+- [1.2-3]
+
+* Mon Apr 24 2006 Stephen Noble <support@dungog.net> 
+- Description Display fixed
+- [1.2-2]
+
+* Fri Apr 14 2006 Stephen Noble <support@dungog.net> 
+- FormMagick Version
+- [1.2-1]
+
+* Thu Apr 13 2006 Stephen Noble <support@dungog.net> 
+- merged sshkeys panel and functions
+- [1.0-13]
+
+* Wed Apr 12 2006 Stephen Noble <support@dungog.net> 
+- possible fix for Insecure dependency on rc1
+- [1.0-12]
+
+* Wed Apr 12 2006 Stephen Noble <support@dungog.net> 
+- option to set VPN Client Access
+- uses user-modify event
+- [1.0-11]
+
+* Thu Feb 23 2006 Stephen Noble <support@dungog.net>
+- proftp.conf/05chroot added as custom template for now
+- defaults all users to ~/home
+- Shell db settings changed was LoginShell
+- [1.0-10]
+
+* Thu Feb 23 2006 Stephen Noble <support@dungog.net>
+- global/individual rssh clarified
+- global reset removed
+- chroot db settings changed
+- [1.0-9]
+
+* Tue Feb 21 2006 Stephen Noble <support@dungog.net>
+- SME 7 pre3 version
+- [1.0-8]
+
+* Thu Aug 11 2005 Stephen Noble <support@dungog.net>
+- SME 7 release
+- [1.0-7]
+
+* Sun Jul 17 2005 Stephen Noble <support@dungog.net>
+- Merge of e-smith-usershellaccess & dungog-chroot-proftp
+- original elements by Daniel van Raay & Damien Curtain
+- [1.0-1]
+
+%prep
+%setup
+
+mkdir -p root/etc/e-smith/events/smeserver-remoteuseraccess-update
+
+%build
+perl createlinks
+
+LEXICONS=$(find root/etc/e-smith/{locale/,web/functions/} -type f )
+
+for lexicon in $LEXICONS
+do
+    /sbin/e-smith/validate-lexicon $lexicon
+done
+
+LINKS=$(find root/etc/e-smith/locale/ -type d -maxdepth 1 | sed 's/root\/etc\/e-smith\/locale\///')
+for link in $LINKS
+do
+ /bin/ln -s remoteuseraccess root/etc/e-smith/locale/$link/etc/e-smith/web/functions/userpanel-sshkeys
+done
+
+%install
+rm -rf $RPM_BUILD_ROOT
+(cd root   ; find . -depth -print | cpio -dump $RPM_BUILD_ROOT)
+rm -f %{name}-%{version}-filelist
+/sbin/e-smith/genfilelist $RPM_BUILD_ROOT > %{name}-%{version}-filelist
+echo "%doc COPYING"          >> %{name}-%{version}-filelist
+
+%clean
+cd ..
+rm -rf %{name}-%{version}
+
+%pre
+%preun
+%post
+
+%postun
+#uninstalls not upgrades
+if [ $1 = 0 ] ; then
+ echo "uninstall"
+fi
+
+%files -f %{name}-%{version}-filelist
+%defattr(-,root,root)
